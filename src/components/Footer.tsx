1034aa362dfc546e7637721c62767fcacccf572d
Schott Taylor <schotttaylor@gmail.com>
2025-09-05 14:13:09 -0600

    Footer styling adjustments

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,8 +8,8 @@ export const Footer = () => {
     <footer id="footer" className="bg-card-foreground dark:bg-accent-foreground text-stone-100 text-sm">
       <hr className="w-full mx-auto border-t-stone-400" />
 
-      <section className="container py-8 grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 xl:grid-cols-6 gap-x-12 gap-y-8">
-        <div className="col-span-full md:col-span-2 justify-center flex">
+      <section className="container py-8 grid grid-cols-3 sm:grid-cols-3 md:grid-cols-5 gap-x-12 gap-y-8">
+        <div className="col-span-full sm:grid-cols-1 md:col-span-2 justify-center flex">
             <a
               rel="noreferrer noopener"
               href="/"
@@ -21,19 +21,6 @@ export const Footer = () => {
                 className="h-full w-auto" 
               />
             </a>
-        </div>
-
-        <div className="flex flex-col">
-          <h3 className="font-medium text-md">Follow US</h3>
-          <div>
-            <a
-              rel="noreferrer noopener"
-              href="https://www.facebook.com/profile.php?id=100063232268373"
-              className="opacity-60 hover:opacity-100"
-            >
-              Facebook
-            </a>
-          </div>
         </div>
 
         <div className="flex flex-col">
@@ -126,18 +113,20 @@ export const Footer = () => {
         </div>
       </section>
 
-      <section className="container pb-4 text-center">
+      <section className="container pb-4 text-center text-xs sm:text-sm">
         <h3>
-          Copyright &copy; 2025 Richfield Area Chamber of Commerce. | 
-          Powered by{" "}
-          <a
-            rel="noreferrer noopener"
-            target="_blank"
-            href="https://openskydev.com"
-            className="text-highlight font-bold transition-all border-primary hover:border-b-2"
-          >
-            Open Sky Solutions
-          </a>
+          Copyright &copy; 2025 Richfield Area Chamber of Commerce | {" "}
+          <span className="text-nowrap">
+            Powered by{" "}
+            <a
+              rel="noreferrer noopener"
+              target="_blank"
+              href="https://openskydev.com"
+              className="text-highlight transition-all border-primary hover:border-b-2"
+            >
+              Open Sky Solutions
+            </a>
+          </span>
         </h3>
       </section>
     </footer>
