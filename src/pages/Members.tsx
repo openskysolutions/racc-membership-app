c249fad55d85c28193417b46bb727aad7adfaf6d
Schott Taylor <schotttaylor@gmail.com>
2025-10-12 19:46:35 -0600

    Clean up

--- src/pages/Members.tsx
+++ src/pages/Members.tsx
@@ -389,7 +389,7 @@ const MembersPage: React.FC = () => {
         <>
           <div className={
             viewMode === 'grid' 
-              ? "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6"
+              ? "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
               : "space-y-2 gap-2"
           }>
             {filteredMembers.map((member) => (
